1f46197efb7897cd53d3fcd856ce093e8b660296
Mateusz Witek <51757549+wazash@users.noreply.github.com>
2022-01-25 20:53:06 +0100

    Dodaj pliki projektów.

---
+++ PrintSkippableMessage.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Async
+{
+    public static class PrintSkippableMessage
+    {
+        public static async Task Print(string message, int timer)
+        {
+            var cancellationTokenSource = new CancellationTokenSource();
+
+            var enterPressedTask = Task.Run(() =>
+            {
+                ConsoleKeyInfo key = Console.ReadKey(true);
+                if (key.Key == ConsoleKey.Enter)
+                {
+                    cancellationTokenSource.Cancel();
+                }
+
+            });
+
+            await SkipMessage(message, timer, cancellationTokenSource.Token);
+
+            await enterPressedTask;
+        }
+
+        private static Task SkipMessage(string message, int timer, CancellationToken cancellationToken)
+        {
+            Task task = null;
+
+            task = Task.Run(() =>
+            {
+                foreach (var item in message)
+                {
+                    Console.Write(item);
+                    Thread.Sleep(timer);
+
+                    if (cancellationToken.IsCancellationRequested)
+                    {
+                        timer = 0;
+                    }
+                }
+            });
+
+            return task;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Async
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            int timer = 50;
+
+            await PrintSkippableMessage.Print("Ala ma kota a Ania ma psa fajnego jakiegoś...\n", timer);
+            await PrintSkippableMessage.Print("Niestety kot Ali i pies Ani nie lubią się...\n", timer);
+            await PrintSkippableMessage.Print("Dlatego ciągle ze sobą walczą...\n", timer);
+        }
+
+        
+    }
+}
